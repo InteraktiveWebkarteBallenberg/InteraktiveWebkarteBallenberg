1e540a55deaf1bbc18c3b6a568cae0263f5b2412
naethu <nathan.matzinger@gmail.com>
2024-09-27 10:25:50 +0200

    Revert "Update next.config.js"
    This reverts commit 4f5a035f7ea5738c82a4adc123efc07a869bf75f.

--- docs/next.config.js
+++ docs/next.config.js
@@ -3,9 +3,4 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = {
-  ...withNextra(),
-  images: {
-    unoptimized: true,
-  },
-};
+module.exports = withNextra()
