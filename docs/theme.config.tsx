7054ca76a5a9db680ffd8c97670c268e69d204ba
naethu <nathan.matzinger@gmail.com>
2024-09-27 10:24:37 +0200

    Revert "update Files"
    This reverts commit 3260b01983f96ce3317c0d66b132e2602452e9cd.

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Interaktive Webkarte Ballenberg</span>,
+  logo: <span>My Project</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
