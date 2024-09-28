347caab3c3534503c8c08c560637e88f7f7fc8c8
naethu <nathan.matzinger@gmail.com>
2024-09-28 13:38:50 +0200

    Update theme.config.tsx

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Interaktive Webkarte Ballenberg</span>,
+  logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/InteraktiveWebkarteBallenberg/InteraktiveWebkarteBallenberg',
+    link: 'https://github.com/shuding/nextra-docs-template',
   },
   chat: {
     link: 'https://discord.com',
