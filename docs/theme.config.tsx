6b640e7111bdc538f8ea12cb41519e57eee15098
naethu <nathan.matzinger@gmail.com>
2024-09-28 16:01:30 +0200

    update docs

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
 
 const config: DocsThemeConfig = {
-  logo: <span>Interaktive Webkarte Ballenberg</span>,
-  project: {
-    link: 'https://github.com/InteraktiveWebkarteBallenberg/InteraktiveWebkarteBallenberg.git',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-  search: {
-    placeholder: 'Suche...',
-  },
-};
+    logo: (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Image
+                src="/images/ballenberg.svg"
+                alt="Interaktive Webkarte Ballenberg"
+                width={40}
+                height={40}
+                style={{ marginRight: '10px' }}
+            />
+            <span>Interaktive Webkarte Ballenberg</span>
+        </div>
+    ),
+    project: {
+        link: 'https://github.com/InteraktiveWebkarteBallenberg/InteraktiveWebkarteBallenberg.git',
+    },
+    docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+    footer: {
+        text: 'Nextra Docs Template',
+    },
+    search: {
+        placeholder: 'Suche...',
+    },
+}
 
 export default config
